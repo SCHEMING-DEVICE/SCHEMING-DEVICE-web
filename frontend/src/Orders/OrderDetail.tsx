cfa5227c0b60ace09add8c0c45889bbe60cdf7ce
Kevin Yang <kevinruolinyang@gmail.com>
2024-02-28 22:21:57 -0500

    Added Order Features

---
+++ frontend/src/Orders/OrderDetail.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import data from "./data.tsx";
+import { useParams } from "react-router-dom";
+
+function OrderDetail() {
+    const { id } = useParams<{ id: string }>();
+    const order = data.orders.find((o) => o._id === parseInt(id as string));
+
+return (
+    <div>
+        <h3>order name: {order?. user.name}</h3>
+    </div>
+);
+}
+export default OrderDetail;
