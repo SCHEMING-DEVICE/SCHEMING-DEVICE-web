07ea944ab8e9bb9bee34b90408d8d9bce95300b0
Jack Witte <witte.85@osu.edu>
2024-04-07 21:06:50 -0400

    lab

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -5,4 +5,23 @@ test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+
+  describe('the main app', ()=> {
+    it('should render the Orders link', () => {
+        render(<App />);
+        expect(screen.getByText('Orders')).toBeInTheDocument();
+    });
+    it('should render the Payments link', () => {
+      render(<App />);
+      expect(screen.getByText('Payments')).toBeInTheDocument();
+  });
+  it('should render the Home link', () => {
+    render(<App />);
+    expect(screen.getByRole('link', {name: "Home"})).toBeInTheDocument();
+});
+it('should render the Home header', () => {
+  render(<App />);
+  expect(screen.getByRole('heading', {level: 2 })).toBeInTheDocument();
+});
+    });
 });
